38e228ef84f7283886d4b3125a01cca18bcb8a29
Igor Mats <igor.mats@yandex.ru>
2013-09-11 04:50:03 +0300

    Added Linux project.

---
+++ Projects/Linux/main.cpp
@@ -0,0 +1,36 @@
+/**
+ * Tooflya Inc. Development
+ * @file AppDelegate.cpp
+ * @category cpp file
+ *
+ * @author Igor Mats from Tooflya Inc.
+ * @copyright (c) 2013 by Igor Mats
+ *
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License v2
+ * @all rights granted under this License are granted for the term of
+ * copyright on the Program, and are irrevocable provided the stated
+ * conditions are met. This License explicitly affirms your unlimited
+ * permission to run the unmodified Program. The output from running a
+ * covered work is covered by this License only if the output, given its
+ * content, constitutes a covered work. This License acknowledges your
+ * rights of fair use or other equivalent, as provided by copyright law.
+ *
+ * @version
+ *
+ */
+
+#include "AppDelegate.h"
+#include "cocos2d.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string>
+
+int main(int argc, char **argv)
+{
+  AppDelegate app;
+  CCEGLView::sharedOpenGLView()->setFrameSize(1136, 640);
+
+  return CCApplication::sharedApplication()->run();
+}
